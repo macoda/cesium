9f8496058452acc17767e6cacf443fe4a9372577
Ravi Agrawal <ravi.agrwl420@gmail.com>
2013-07-13 15:37:01 +0530

    basic code added to Navigation.js and NavigationViewModel.js

---
+++ Source/Widgets/Navigation/Navigation.js
@@ -0,0 +1,26 @@
+/*global define*/
+define([
+        '../../Core/defaultValue',
+        '../../Core/defineProperties',
+        '../../Core/destroyObject',
+        '../../Core/DeveloperError',
+        '../getElement',
+        '../../ThirdParty/knockout'
+    ], function(
+        defaultValue,
+        defineProperties,
+        destroyObject,
+        DeveloperError,
+        getElement,
+        knockout) {
+    "use strict";
+
+    var svgNS = "http://www.w3.org/2000/svg";
+    var xlinkNS = "http://www.w3.org/1999/xlink";
+
+    var Navigation = function() {
+
+    };
+
+    return Navigation;
+});
---
+++ Source/Widgets/Navigation/NavigationViewModel.js
@@ -0,0 +1,23 @@
+/*global define*/
+define([
+        '../../Core/defineProperties',
+        '../../Core/DeveloperError',
+        '../createCommand',
+        '../ToggleButtonViewModel',
+        '../../ThirdParty/sprintf',
+        '../../ThirdParty/knockout'
+    ], function(
+        defineProperties,
+        DeveloperError,
+        createCommand,
+        ToggleButtonViewModel,
+        sprintf,
+        knockout) {
+    "use strict";
+
+    var NavigationViewModel = function() {
+
+    };
+
+    return NavigationViewModel;
+});
